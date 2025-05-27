097bae5e5bb1f85ee94d63c97d62669ce9131021
Yonatan Ben Ami <yba6081@gmail.com>
2025-05-27 11:52:14 +0300

    add: testing for classes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Commandos
+﻿using Commandos.Models;
+namespace Commandos
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            //testing 
+            Commado commado1 = new Commado("Yonatan", 123);
+            commado1.Attack();
+            Console.WriteLine($"{commado1.Name} - {commado1.CodeName} - {commado1.Status}");
+            Weapon weapon1 = new Weapon("Mag", "USA", 50);
+            weapon1.Shoot();
+            Console.WriteLine($"{weapon1.Name} - {weapon1.Producer} - {weapon1.NumBullets}");
         }
     }
 }
