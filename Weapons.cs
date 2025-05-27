0be18106723270d307b51ba50fe431405975095e
Yonatan Ben Ami <yba6081@gmail.com>
2025-05-27 11:06:21 +0300

    add: base weapons class

---
+++ Weapons.cs
@@ -0,0 +1,11 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace Commandos.Models
+{
+    public class Weapon
+    {
+        public string Name;
+        public string producer;
+        public int NumberBullets;
+    }
+}
